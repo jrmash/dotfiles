46254f814c01869e0ffdf0bcc95590d5ab6b9ff5
J.R. Mash <2574997+jrmash@users.noreply.github.com>
2025-10-27 22:23:19 -0700

    Improve Homebrew Bootstrapping
    Refactor Homebrew installation logic for better platform and architecture handling.
    
    Signed-off-by: J.R. Mash <2574997+jrmash@users.noreply.github.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env -S bash -euET -o pipefail
 
+arch="$(uname -m)"
+platform="$(uname -s)"
+
 ## Install and setup Homebrew (if missing) and enable it in the current shell.
 if [ ! "$(command -v brew)" > /dev/null ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  
-  if [ -d "/opt/homebrew" ]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
-  else
-    eval "$(/usr/local/bin/brew shellenv)"
-  fi
+
+  if [[ "${platform}" =~ ^Darwin ]]; then
+    if [[ "${arch}" == "arm64" ]]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    elif [[ "${arch}" == "x86_64" ]]; then
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
+  elif [[ "${platform}" =~ ^Linux ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  fi  
 fi
 
 brew install chezmoi
